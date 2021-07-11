597ff9ede05d74ceab2e39eb5271666b385ca482
Jay Dhamsaniya <jay.dhamsaniya@darshan.ac.in>
2021-07-11 15:53:13 +0530

    BookShow and Confirmation Screens added in Controller.js

--- src/screens/Controller.js
+++ src/screens/Controller.js
@@ -2,6 +2,8 @@ import React from 'react';
 import {BrowserRouter as Router, Route} from 'react-router-dom';
 import Home from './home/Home';
 import Details from './details/Details';
+import BookShow from './bookshow/BookShow';
+import Confirmation from './confirmation/Confirmation';
 
 export default function Controller() {
 
@@ -12,6 +14,8 @@ export default function Controller() {
             <div className="main-container">
                 <Route exact path="/" render={(props) => <Home {...props} baseUrl={baseUrl}/>}/>
                 <Route exact path="/movie/:id" render={(props) => <Details {...props} baseUrl={baseUrl}/>} />
+                <Route exact path="/bookshow/:id" render={(props) => <BookShow {...props} baseUrl={baseUrl}/>} />
+                <Route exact path="/confirm/:id" render={(props) => <Confirmation {...props} baseUrl={baseUrl}/>} />
             </div>
         </Router>
     );
